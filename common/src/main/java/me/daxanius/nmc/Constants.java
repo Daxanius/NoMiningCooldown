33c1cc9653fc5e4e9999a33d983722c2ad342669
Daxanius <balder.huybreghs@gmail.com>
2025-10-07 17:21:25 +0200

    Migrate project to multi-loader structure

---
+++ common/src/main/java/me/daxanius/nmc/Constants.java
@@ -0,0 +1,10 @@
+package me.daxanius.nmc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+    public static final String MOD_ID = "nmc";
+    public static final String MOD_NAME = "NoMiningCooldown";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
--- fabric/src/main/java/me/daxanius/nmc/NoMiningCooldownClient.java
+++ fabric/src/main/java/me/daxanius/nmc/NoMiningCooldownClient.java
@@ -1,17 +1,17 @@
 package me.daxanius.nmc;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.network.chat.Component;
 import org.lwjgl.glfw.GLFW;
 
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
 public class NoMiningCooldownClient implements ClientModInitializer {
     // Keybindings
-    private static KeyBinding toggleCooldownKeyBinding;
+    private static KeyMapping toggleCooldownKeyBinding;
 
     // Variables
     public static boolean cooldownFixEnabled = true;
@@ -19,23 +19,23 @@ public class NoMiningCooldownClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // Register keybinds
-        toggleCooldownKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.nmc.toggle",    // Keybinding translation
-                InputUtil.Type.KEYSYM,   // Input type
-                GLFW.GLFW_KEY_G,         // Keycode
-                KeyBinding.Category.MISC // Category translation
+        toggleCooldownKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key.nmc.toggle",     // Keybinding translation
+                InputConstants.Type.KEYSYM, // Input type
+                GLFW.GLFW_KEY_G,            // Keycode
+                KeyMapping.CATEGORY_MISC    // Category translation
         ));
 
         // Add key listener
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (toggleCooldownKeyBinding.wasPressed()) {
+            while (toggleCooldownKeyBinding.consumeClick()) {
                 if (client.player == null) {
                     continue;
                 }
 
                 // Toggle the cool-down fix
                 cooldownFixEnabled = !cooldownFixEnabled;
-                client.player.sendMessage(Text.translatable(cooldownFixEnabled ? "message.nmc.cooldown_disabled" : "message.nmc.cooldown_enabled"), true);
+                client.player.displayClientMessage(Component.translatable(cooldownFixEnabled ? "message.nmc.cooldown_disabled" : "message.nmc.cooldown_enabled"), true);
             }
         });
     }
