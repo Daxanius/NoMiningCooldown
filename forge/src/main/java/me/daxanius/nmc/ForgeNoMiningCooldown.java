8f81a46bae66f63dffd971d13b5b0a2dfeeffe36
Daxanius <balder.huybreghs@gmail.com>
2025-10-07 22:39:18 +0200

    Fix forge keybind

--- forge/src/main/java/me/daxanius/nmc/ForgeNoMiningCooldown.java
+++ forge/src/main/java/me/daxanius/nmc/ForgeNoMiningCooldown.java
@@ -1,17 +1,11 @@
 package me.daxanius.nmc;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(value = Constants.MOD_ID)
 public class ForgeNoMiningCooldown {
     public ForgeNoMiningCooldown(FMLJavaModLoadingContext context) {
-        var modBusGroup = context.getModBusGroup();
-        FMLClientSetupEvent.getBus(modBusGroup).addListener(this::onClientSetup);
-    }
-
-    private void onClientSetup(FMLClientSetupEvent event) {
-        NoMiningCooldownClient.init();
+        // We love forge (not)
     }
 }
--- forge/src/main/java/me/daxanius/nmc/platform/ForgeClientInputHelper.java
+++ forge/src/main/java/me/daxanius/nmc/platform/ForgeClientInputHelper.java
@@ -1,6 +1,7 @@
 package me.daxanius.nmc.platform;
 
 import me.daxanius.nmc.Constants;
+import me.daxanius.nmc.NoMiningCooldownClient;
 import me.daxanius.nmc.platform.services.IClientInputHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
@@ -13,11 +14,17 @@ import java.util.List;
 
 @Mod.EventBusSubscriber(modid = Constants.MOD_ID)
 public class ForgeClientInputHelper implements IClientInputHelper {
+    private static boolean initialized = false;
     private static final List<Runnable> tickCallbacks = new ArrayList<>();
     private static final List<KeyMapping> keyMappings = new ArrayList<>();
 
     @SubscribeEvent
     public static void registerBindings(RegisterKeyMappingsEvent event) {
+        if (!initialized) {
+            NoMiningCooldownClient.init(); // Initialize the client before bindings lol, we love forges
+            initialized = true;
+        }
+
         for (KeyMapping key : keyMappings) {
             event.register(key);
         }
